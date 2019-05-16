a292e44d5262599f7fd215d3eafdd4e147c89384
eisendaniel <38846425+eisendaniel@users.noreply.github.com>
2019-05-17 11:44:46 +1200

    added current c++ build

---
+++ files_cpp/cmake-build-debug/CMakeFiles/t10test_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/t10test_cpp.dir/t10test.cc.obj"
+  "CMakeFiles/t10test_cpp.dir/editor2.cc.obj"
+  "t10test_cpp.pdb"
+  "t10test_cpp.exe"
+  "t10test_cpp.exe.manifest"
+  "libt10test_cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/t10test_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ files_cpp/cmake-build-debug/CMakeFiles/t11test_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/t11test_cpp.dir/t11test.cc.obj"
+  "CMakeFiles/t11test_cpp.dir/editor2.cc.obj"
+  "t11test_cpp.pdb"
+  "t11test_cpp.exe"
+  "t11test_cpp.exe.manifest"
+  "libt11test_cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/t11test_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

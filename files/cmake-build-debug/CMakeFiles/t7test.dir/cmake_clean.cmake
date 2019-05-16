347a22684440999d7fb1c532dbe0593b32d8f752
eisendaniel <38846425+eisendaniel@users.noreply.github.com>
2019-05-17 11:36:42 +1200

    initial upload [C]

---
+++ files/cmake-build-debug/CMakeFiles/t7test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/t7test.dir/t7test.c.obj"
+  "CMakeFiles/t7test.dir/editor2.c.obj"
+  "t7test.pdb"
+  "t7test.exe"
+  "t7test.exe.manifest"
+  "libt7test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/t7test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ files/cmake-build-debug/CMakeFiles/t8test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/t8test.dir/t8test.c.obj"
+  "CMakeFiles/t8test.dir/editor2.c.obj"
+  "t8test.pdb"
+  "t8test.exe"
+  "t8test.exe.manifest"
+  "libt8test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/t8test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
